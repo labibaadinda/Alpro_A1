8885fcc37a24547ef091121353532caf88068c99
labibaadinda <100503649+labibaadinda@users.noreply.github.com>
2022-03-02 20:35:55 +0700

    Add files via upload

---
+++ Praktikum 2/bujur sangkar.c
@@ -0,0 +1,27 @@
+/*Nama File : [bujur sangkar].c*/
+/*Deskripsi : [Menghitung luas dari bujung sangkar]*/
+/*Pembuat   : [Labiba Adinda Zahwana - 24060121140111]*/
+/*Tanggal   : [Selasa, 01-03-2022 11:54 WIB]*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main()
+
+{
+
+/*Kamus*/;
+   int s;
+   int l;
+/*Algoritma*/
+   printf("Masukkan sisi dari bujur sangkar = ");
+   scanf("%d", &s);
+//s*s
+   l = s*s;
+   printf("Luas Bujur Sangkar = %d satuan luas", l);
+
+   return 0;
+
+}
